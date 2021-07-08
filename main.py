1beea5f187860d0f73d8a146605291661467b3b0
Will Spencer <willspencer16@gmail.com>
2021-07-08 11:47:17 +0100

    Completely refactored. Programme currently only adds duration to start without showing day changes.

--- main.py
+++ main.py
@@ -3,8 +3,5 @@ from time_calculator import add_time
 from unittest import main
 
 
-print(add_time("11:06 PM", "2:02"))
-
-
 # Run unit tests automatically
-# main(module='test_module', exit=False)
+main(module='test_module', exit=False)
--- time_calculator.py
+++ time_calculator.py
@@ -1,30 +1,8 @@
-def add_time(start, duration):
-    start_input = start.split()
-    start_meridiem = start_input[1]
-    start_time = start_input[0].split(':')
-    start_hours = start_time[0]
-    start_minutes = start_time[1]
+from datetime import *
 
-    duration_input = duration.split()
-    duration_time = duration_input[0].split(':')
-    duration_hours = duration_time[0]
-    duration_minutes = duration_time[1]
-
-    if start_meridiem == 'PM':
-        start_hours = int(start_hours) + 12
-    
-    new_hours = int(start_hours) + int(duration_hours)
-
-    if new_hours > 24:
-        new_hours = int(new_hours) - 24
-        new_meridiem = 'AM'
-    else:
-        new_hours = int(new_hours) - 12
-        new_meridiem = start_meridiem
-    
-
-    new_minutes = int(start_minutes) + int(duration_minutes)
-
-    new_time = f"{new_hours}:{new_minutes:02d} {new_meridiem}"
+def add_time(start, duration, day="Monday"):
+    start = datetime.strptime(start, '%I:%M %p')
+    duration = timedelta(minutes=(int(duration.split(":")[0]) * 60) + int(duration.split(":")[1]))
+    new_time = (start + duration).strftime("%-I:%M %p")
     
     return new_time
